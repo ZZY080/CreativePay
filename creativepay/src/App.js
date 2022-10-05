e991ecb0183b36f58df2392b70d704d39ad8c896
zengzhiyuan <2916363651@qq.com>
2022-10-05 18:41:45 +0800

    完成了个人中心开发

--- creativepay/src/App.js
+++ creativepay/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { NavLink, Route } from "react-router-dom"
 import Header from './compontents/Header/index.jsx'
-import MainIndex from "./compontents/MainIndex/index.jsx"
-// import  MainPersonCenter from './compontents/MainPersonCenter/index.jsx'
+// import MainIndex from "./compontents/MainIndex/index.jsx"
+import  MainPersonCenter from './compontents/MainPersonCenter/index.jsx'
 import './App.css'
 
 export default class App extends Component {
@@ -10,8 +10,8 @@ export default class App extends Component {
     return (
       <div>
         <Header />
-        <MainIndex/>   
-        {/* <MainPersonCenter/> */}
+        {/* <MainIndex/>    */}
+        <MainPersonCenter/>
       </div>
     )
   }
--- creativepay/src/compontents/MainPersonCenter/index.jsx
+++ creativepay/src/compontents/MainPersonCenter/index.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
-import 'antd/dist/antd.css'
-import { Row, Col, Card } from 'antd'
 import "./index.css"
 export default class index extends Component {
     render() {
         return (
             <div className='MainPersonCenter'>
+                <div className="MainPersonCenter-header">
+                    登录账号信息
+                </div>
+                <div className="MainPersonCenter-footer">
+                    <ul>
+                        <li>
+                            <span>昵称</span>
+                            <input type="text" placeholder='何**'/>
+                        </li>
+                        <li>
+                            <span>用户名</span>
+                            <input type="text" placeholder='29163636**' />
+                            <button>变更</button>
+                        </li>
+                        <li>
+                            <span>密码</span>
+                            <input type="password" placeholder='15151**' />
+                            <button>变更</button>
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }
